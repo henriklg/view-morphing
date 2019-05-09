1ad8d2ebb05d2f9d6e2a084b0e668d639520b23d
henriklg <henrik.logj@gmail.com>
2019-05-09 12:52:08 +0200

    removed uneccessary while loop

--- main_script.py
+++ main_script.py
@@ -5,8 +5,6 @@ import numpy as np
 from numpy.linalg import inv
 
 
-
-
 image1 = cv2.imread('data/einstein1.jpg')
 image2 = cv2.imread('data/einstein3.jpg')
 F = findfundmat(image1, image2)
@@ -19,14 +17,11 @@ print('H0: ', H0)
 print('H1: ', H1)
 
 width, height, _ = image1.shape
-dest = np.zeros(image1.shape)
 dest = cv2.warpPerspective(image1, inv(H0), (width, height))
 dest2 = cv2.warpPerspective(image2, inv(H0), (width, height))
 
-while(True):
-    cv2.imshow('wind', dest)
-    cv2.imshow('wind2', dest2)
+cv2.imshow('wind', dest)
+cv2.imshow('wind2', dest2)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    break
+cv2.waitKey(0)
+cv2.destroyAllWindows()
