b9dfed81c7a532fecfc06cc47823b20dfb3caaa1
henriklg <henrik.logj@gmail.com>
2019-05-16 10:38:32 +0200

    merged two files and rename

--- find_fundamental.py
+++ find_fundamental.py
@@ -1,17 +1,48 @@
 import numpy as np
-from normalize_points import normalize_points
 from pointCorrespondences import automatic_point_correspondences
 
-def findfundmat(image1, image2):
+
+def normalize_points(x, y):
+    """
+    function computing normalization (translation and scaling) of the coordinates of the matched points
+
+    :param x; (n x 1 ndarray) x coordinates of points
+    :param y: (n x 1 ndarray) y coordinates of points
+    :return: normalized points and 3x3 transformation matrix
+    """
+    assert
+    num_points = len(x)
+
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+
+    shifted_x = x - mean_x
+    shifted_y = y - mean_y
+
+    sf = np.sqrt(2) / np.mean( np.sqrt(shifted_x**2 + shifted_y**2) )
+    # 3x3 transformation matrix
+    t_norm = np.array([[sf, 0, -sf*mean_x], [0, sf, -sf*mean_y], [0, 0, 1]])
+
+    o = np.ones((num_points, 1))
+
+    xy = np.concatenate((x, y, o), axis=1)
+
+    # (n x 3) normalized homogeneous coordinates of points
+    xy_normalized = np.dot(t_norm, xy.T)
+
+    return xy_normalized.T, t_norm
+
+
+
+def fundamental_matrix(image1, image2):
     """
     calculates a fundamental matrix for the two specified images
 
     :param image1:
     :param image2:
-    :return:
+    :return: fundamental matrix F (3 x 3)
     """
     [x1, y1, x2, y2] = automatic_point_correspondences(image1, image2)
-    #F = np.zeros((3, 3))
 
     # get the number of data points
     num_points = len(x1)
@@ -48,8 +79,8 @@ def findfundmat(image1, image2):
 
 
 if __name__ == '__main__':
-    image1 = 'einstein1.jpg'
-    image2 = 'einstein3.jpg'
+    image1 = 'data/einstein1.jpg'
+    image2 = 'data/einstein3.jpg'
 
     fundamental_matrix = findfundmat(image1, image2)
     print(fundamental_matrix)
